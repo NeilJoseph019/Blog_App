1092fc898ce5e1fa0e5a660422707ec23aa6cd9e
Neil Joseph <neiljoseph@Neils-MacBook-Pro.local>
2022-09-26 09:32:29 +0100

    global and local templates created, added blog to APPS list, created views, and added urls.

--- BlogApp/blog/urls.py
+++ BlogApp/blog/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    path(),
+    path("", views.indexPage, name="indexPage"),
+    path("posts", views.postsPage, name="postsPage"),
+    path("posts/<slug:slug>", views.postDetailPage, name="postDetailPage"),
 ]
--- BlogApp/blog/views.py
+++ BlogApp/blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+def indexPage(request):
+    s = "Hello there lads "
+    context = {
+        "hello" : s
+    }    
+    return render(request,"blog/index.html", context)
+
+def postsPage(request):
+    pass
+
+def postDetailPage(request):
+    pass

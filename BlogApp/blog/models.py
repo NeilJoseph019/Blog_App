c42f13d94ffeb843087fd72c113d91420a8cf05b
Neil Joseph <neiljoseph@Neils-MacBook-Pro.local>
2022-10-01 23:11:59 +0100

    Second phase of the project is completed: Created a model, added data to the model, created superuser for the admin panel and updated the views.py file.

--- BlogApp/blog/models.py
+++ BlogApp/blog/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
-# Create your models here.
+class Author(models.Model):
+    first_name = models.CharField(max_length=120)
+    last_name =models.CharField(max_length=120)
+    email_address = models.EmailField()
+
+    def __str__(self):
+        return f" {self.first_name} {self.last_name}"
+
+class Tag(models.Model):
+    caption = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.caption
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    brief_description = models.CharField(max_length=240)
+    content = models.TextField(validators=[MinLengthValidator(10)])
+    image = models.CharField(max_length=100)
+    timestamp = models.DateTimeField(auto_now=True)
+    slug = models.SlugField(unique=True, db_index=True)
+
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL,null=True, related_name="author")
+    tag = models.ManyToManyField(Tag, related_name="tag")
+
+    def __str__(self):
+        return self.title
+    
--- BlogApp/blog/views.py
+++ BlogApp/blog/views.py
@@ -1,80 +1,83 @@
-from django.shortcuts import render
-from django.urls import reverse_lazy
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
-posts = [
-    {
-        "slug": "the-mountains",
-        "image":"AdventureVacations.jpeg",
-        "title": "Hiking Trip",
-        "author":"Neil",
-        "timeStamp": "09-27-2022",
-        "briefDescription": "This was an adventurous trip i had back in 2019 during the pandamic along with my friends.Had lots of stories to tell and memories made. The view in the morning from the hill was simply breath-taking. You must camp atleast once in your life time.",
-        "content":"""
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta, delectus. Hic laudantium eius placeat eum natus corrupti praesentium, a, 
-        doloremque provident quod reprehenderit iusto, earum cum voluptatibus velit perspiciatis ratione!
+# posts = [
+#     {
+#         "slug": "the-mountains",
+#         "image":"AdventureVacations.jpeg",
+#         "title": "Hiking Trip",
+#         "author":"Neil",
+#         "timeStamp": "09-27-2022",
+#         "briefDescription": "This was an adventurous trip i had back in 2019 during the pandamic along with my friends.Had lots of stories to tell and memories made. The view in the morning from the hill was simply breath-taking. You must camp atleast once in your life time.",
+#         "content":"""
+#         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta, delectus. Hic laudantium eius placeat eum natus corrupti praesentium, a, 
+#         doloremque provident quod reprehenderit iusto, earum cum voluptatibus velit perspiciatis ratione!
 
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta, delectus. Hic laudantium eius placeat eum natus corrupti praesentium, a, 
-        doloremque provident quod reprehenderit iusto, earum cum voluptatibus velit perspiciatis ratione!
+#         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta, delectus. Hic laudantium eius placeat eum natus corrupti praesentium, a, 
+#         doloremque provident quod reprehenderit iusto, earum cum voluptatibus velit perspiciatis ratione!
+
+#         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta, delectus. Hic laudantium eius placeat eum natus corrupti praesentium, a, 
+#         doloremque provident quod reprehenderit iusto, earum cum voluptatibus velit perspiciatis ratione!
+#         """
+#     },
+#     {
+#         "slug": "camping-trip",
+#         "image":"campingVacation.jpeg",
+#         "title": "Camping Trip",
+#         "author":"Neil",
+#         "timeStamp": "10-18-2022",
+#         "briefDescription": "This was an adventurous trip i had back in 2019 during the pandamic along with my friends.Had lots of stories to tell and memories made. The view in the morning from the hill was simply breath-taking. You must camp atleast once in your life time.",
+#         "content":"""
+#         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta, delectus. Hic laudantium eius placeat eum natus corrupti praesentium, a, 
+#         doloremque provident quod reprehenderit iusto, earum cum voluptatibus velit perspiciatis ratione!
+
+#         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta, delectus. Hic laudantium eius placeat eum natus corrupti praesentium, a, 
+#         doloremque provident quod reprehenderit iusto, earum cum voluptatibus velit perspiciatis ratione!
         
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta, delectus. Hic laudantium eius placeat eum natus corrupti praesentium, a, 
-        doloremque provident quod reprehenderit iusto, earum cum voluptatibus velit perspiciatis ratione!
-        """
-    },
-    {
-        "slug": "camping-trip",
-        "image":"campingVacation.jpeg",
-        "title": "Camping Trip",
-        "author":"Neil",
-        "timeStamp": "10-18-2022",
-        "briefDescription": "This was an adventurous trip i had back in 2019 during the pandamic along with my friends.Had lots of stories to tell and memories made. The view in the morning from the hill was simply breath-taking. You must camp atleast once in your life time.",
-        "content":"""
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta, delectus. Hic laudantium eius placeat eum natus corrupti praesentium, a, 
-        doloremque provident quod reprehenderit iusto, earum cum voluptatibus velit perspiciatis ratione!
+#         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta, delectus. Hic laudantium eius placeat eum natus corrupti praesentium, a, 
+#         doloremque provident quod reprehenderit iusto, earum cum voluptatibus velit perspiciatis ratione!
+#         """
+#     },
+#     {
+#         "slug": "resort",
+#         "image":"resort.jpeg",
+#         "title": "Staycation Trip",
+#         "author":"Neil",
+#         "timeStamp": "11-20-2022",
+#         "briefDescription": "This was an adventurous trip i had back in 2019 during the pandamic along with my friends.Had lots of stories to tell and memories made. The view in the morning from the hill was simply breath-taking. You must camp atleast once in your life time.",
+#         "content":"""
+#         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta, delectus. Hic laudantium eius placeat eum natus corrupti praesentium, a, 
+#         doloremque provident quod reprehenderit iusto, earum cum voluptatibus velit perspiciatis ratione!
 
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta, delectus. Hic laudantium eius placeat eum natus corrupti praesentium, a, 
-        doloremque provident quod reprehenderit iusto, earum cum voluptatibus velit perspiciatis ratione!
+#         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta, delectus. Hic laudantium eius placeat eum natus corrupti praesentium, a, 
+#         doloremque provident quod reprehenderit iusto, earum cum voluptatibus velit perspiciatis ratione!
         
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta, delectus. Hic laudantium eius placeat eum natus corrupti praesentium, a, 
-        doloremque provident quod reprehenderit iusto, earum cum voluptatibus velit perspiciatis ratione!
-        """
-    },
-    {
-        "slug": "resort",
-        "image":"resort.jpeg",
-        "title": "Staycation Trip",
-        "author":"Neil",
-        "timeStamp": "11-20-2022",
-        "briefDescription": "This was an adventurous trip i had back in 2019 during the pandamic along with my friends.Had lots of stories to tell and memories made. The view in the morning from the hill was simply breath-taking. You must camp atleast once in your life time.",
-        "content":"""
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta, delectus. Hic laudantium eius placeat eum natus corrupti praesentium, a, 
-        doloremque provident quod reprehenderit iusto, earum cum voluptatibus velit perspiciatis ratione!
-
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta, delectus. Hic laudantium eius placeat eum natus corrupti praesentium, a, 
-        doloremque provident quod reprehenderit iusto, earum cum voluptatibus velit perspiciatis ratione!
-        
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta, delectus. Hic laudantium eius placeat eum natus corrupti praesentium, a, 
-        doloremque provident quod reprehenderit iusto, earum cum voluptatibus velit perspiciatis ratione!
-        """
-    }
-]
+#         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta, delectus. Hic laudantium eius placeat eum natus corrupti praesentium, a, 
+#         doloremque provident quod reprehenderit iusto, earum cum voluptatibus velit perspiciatis ratione!
+#         """
+#     }
+# ]
 
 # Create your views here.
 def indexPage(request):
-    displayPosts = posts[-1:]
+    # displayPosts = posts[-1:]
+    displayPosts = Post.objects.all().order_by("-timestamp")[:2]
     context = {
         "posts" : displayPosts
     }    
     return render(request,"blog/index.html", context)
 
 def blogsPage(request):
-    allPosts = posts
+    allPosts = Post.objects.all().order_by("-timestamp")
     context = {
         "allPosts" : allPosts
     }    
     return render(request,"blog/blogsPage.html",context)
 
 def blogDetailPage(request, slug):
-    identifiedPost = next(post for post in posts if post["slug"] == slug) # next() finds the next element that matches a certain condition. The condition that is met is list comprehension.
+    identifiedPost = get_object_or_404(Post, slug = slug)
+    # # identifiedPost = Post.objects.get(slug=slug)
+    # identifiedPost = next(post for post in posts if post["slug"] == slug) # next() finds the next element that matches a certain condition. The condition that is met is list comprehension.
     context = {
         "post": identifiedPost
     }
